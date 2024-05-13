c287b52a81c89f14f361df4b4be2f36c16110abe
5kj41d <carlsen57@gmail.com>
2024-05-13 11:20:37 +0200

    Added script to run the GAN live and generate n number of images

---
+++ run_gan_script.py
@@ -0,0 +1,21 @@
+import torch
+from torchvision.utils import save_image
+import os
+
+subfolder= "models"
+model_file_name = "gan_1st_script" 
+model_path = os.path.join(subfolder, model_file_name) 
+
+model_scripted = torch.jit.load(model_path)
+model_scripted.eval()
+
+output_folder = "./live_generated_images_gan"
+os.makedirs(output_folder, exist_ok = True)
+
+num_images = 100 
+for i in range(num_images):
+    noise = torch.randn(1, 100, 1, 1) 
+    generated_image = model_scripted(noise)
+    save_image(generated_image, os.path.join(output_folder, f"generated_image_{i+1}.png"))
+
+print(f"{num_images} images generated and saved to {output_folder}.")
